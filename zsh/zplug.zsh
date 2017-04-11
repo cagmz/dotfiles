4b293c288f3868235a282dbc3cd138b8e98f074e
Carlos Adrian Gomez <cagomez805@gmail.com>
2017-04-10 18:59:11 -0700

    Removed some zsh plugins

--- zsh/zplug.zsh
+++ zsh/zplug.zsh
@@ -9,11 +9,6 @@ if [ -z ${RELOAD} ]; then
     zplug 'zplug/zplug', hook-build:'zplug --self-manage'
     zplug 'zsh-users/zsh-syntax-highlighting', defer:2
     zplug 'zsh-users/zsh-autosuggestions'
-    zplug 'akoenig/npm-run.plugin.zsh'
-    zplug 'yonchu/grunt-zsh-completion'
-
-    export NVM_LAZY_LOAD=true
-    zplug "lukechilds/zsh-nvm"
 
     # Install plugins if there are plugins that have not been installed
     if ! zplug check; then
